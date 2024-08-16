0248d571ad0c43ebeb12642805dfadd0450d99f9
RaminKh <kh.ramin69@gmail.com>
2024-08-16 12:02:34 +0330

    feat(screen): :sparkles: add signup screen

---
+++ src/screens/auth/Signup/index.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+import {styles} from './styles';
+import AuthHeader from '../../../components/AuthHeader';
+import Input from '../../../components/Input';
+import Button from '../../../components/Button';
+import CheckBox from '../../../components/CheckBox';
+import GoogleButton from '../../../components/GoogleButton';
+import {useForm, Controller, SubmitHandler} from 'react-hook-form';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../../../../App';
+
+type Props = NativeStackScreenProps<RootStackParamList, 'SignUp'>;
+
+type SignUpFormProps = {
+  name: string;
+  email: string;
+  password: string;
+};
+interface SignUpProps {}
+
+const SignUp = ({navigation}: Props) => {
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm<SignUpFormProps>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<SignUpFormProps> = data => {
+    console.log(data);
+  };
+
+  return (
+    <View style={styles.container}>
+      <AuthHeader
+        title="Sign Up"
+        onBack={() => navigation.navigate('Splash', {})}
+      />
+      <View style={styles.form}>
+        <Controller
+          control={control}
+          name="name"
+          rules={{required: true}}
+          render={({field: {value, onChange}}) => (
+            <Input
+              label="Name"
+              placeHolder="John Doe"
+              value={value}
+              onChange={onChange}
+              error={Boolean(errors.name)}
+            />
+          )}
+        />
+        <Controller
+          control={control}
+          name="email"
+          rules={{required: true}}
+          render={({field: {value, onChange}}) => (
+            <Input
+              label="E-mail"
+              placeHolder="johnDoe@gmail.com"
+              value={value}
+              onChange={onChange}
+              error={Boolean(errors.email)}
+            />
+          )}
+        />
+        <Controller
+          control={control}
+          name="password"
+          rules={{required: true}}
+          render={({field: {value, onChange}}) => (
+            <Input
+              label="Password"
+              placeHolder="*********"
+              value={value}
+              onChange={onChange}
+              password
+              error={Boolean(errors.password)}
+            />
+          )}
+        />
+        <View style={styles.bottom}>
+          <CheckBox
+            _renderLabel={() => (
+              <Text style={styles.termsAndPrivacy}>
+                I agree with <Text style={styles.strong}>Terms</Text> &{' '}
+                <Text style={styles.strong}>Privacy</Text>
+              </Text>
+            )}
+          />
+          <Button title="Sign up" onPress={handleSubmit(onSubmit)} />
+          <View style={styles.signUpWith}>
+            <View style={styles.line} />
+            <Text style={styles.signUpWithText}>Or Sign up with</Text>
+            <View style={styles.line} />
+          </View>
+          <GoogleButton onPress={() => {}} />
+          <Text style={styles.already}>
+            Already have an account? <Text style={styles.signIn}>Sing In</Text>
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default SignUp;
